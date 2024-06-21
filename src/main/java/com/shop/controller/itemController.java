af96471419db939ee9242a7f1ac47042055d5cb6
yunju94 <qkryj1004@namver.com>
2024-06-21 18:11:04 +0900

    _06_21_금요일

--- src/main/java/com/shop/controller/itemController.java
+++ src/main/java/com/shop/controller/itemController.java
@@ -110,6 +110,14 @@ public class itemController {
         return "item/itemMng";
     }
 
+    @GetMapping(value = "/item/{itemId}")
+    public String itemDtl(Model model, @PathVariable("itemId")Long itemId){
+        ItemFormDto itemFormDto = itemService.getItemDtl(itemId);
+        model.addAttribute("item", itemFormDto);
+        return "item/itemDtl";
+    }
+
+
 
 
 
--- src/main/java/com/shop/entity/Order.java
+++ src/main/java/com/shop/entity/Order.java
@@ -36,4 +36,39 @@ public class Order extends  BaseEntity{
     //private LocalDateTime regTime;
 
     //private  LocalDateTime updateTime;
+
+    //주문서 주문 아이템 리스트에 주문 아이템 주가
+    //주문 아이템에 주문서 추가
+    public  void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+    //주문서 생성
+    //현재 로그인된 멤버 주문서에 추가
+    //주문 아이템 리스트를 반복문을 통해서 주문서에 추가
+    //상태는 주문으로 세팅
+    //주문 시간은 현재시간으로 세팅
+    //주문서 리턴
+    public  static  Order createOrder(Member member, List<OrderItem> orderItemList){
+        Order order = new Order();
+        order.setMember(member);
+        for (OrderItem orderItem : orderItemList){
+            order.addOrderItem(orderItem);
+        }
+        order.setOrderStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return  order;
+
+    }
+    //주문서에 있는 주문 아이템 리스트를 반복
+    //주문 아이템 마다 총 가격을 totalPrice에 추가
+    public  int getTotalPrice(){
+        int totalPrice = 0 ;
+        for (OrderItem orderItem : orderItems){
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
+
+
 }
